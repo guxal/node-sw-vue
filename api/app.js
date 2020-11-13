fa0e0f4176f421e341d4eb42bc3e84158b4978e2
guxal <jonathanacp93@gmail.com>
2020-11-13 17:52:00 -0500

    delete demo code

--- api/app.js
+++ api/app.js
@@ -5,8 +5,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var peopleRouter = require('./routes/people');
 
 var HOST_NAME = 'db';
@@ -22,10 +20,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use('/people', peopleRouter);
 
